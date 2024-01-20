c3ecd6bd7843ed24de61c769871cf083190a113d
Shadab Khan <shadabkhan7253@gmail.com>
2024-01-20 12:32:23 +0530

    price updated and key added to uniquely identify MeaItem component

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,31 +7,37 @@ const DUMMY_MEALS = [
     id: 'm1',
     name: 'Sushi',
     description: 'Finest fish and veggies',
-    price: 22.99,
+    price: 650.55,
   },
   {
     id: 'm2',
     name: 'Schnitzel',
     description: 'A german specialty!',
-    price: 16.5,
+    price: 450.25,
   },
   {
     id: 'm3',
     name: 'Barbecue Burger',
     description: 'American, raw, meaty',
-    price: 12.99,
+    price: 240.99,
   },
   {
     id: 'm4',
     name: 'Green Bowl',
     description: 'Healthy...and green...',
-    price: 18.99,
+    price: 149.49,
   },
 ];
 
 const AvailableMeals = (props) => {
   const mealsList = DUMMY_MEALS.map((meal) => (
-    <MealItem id={meal.id} name={meal.name} description={meal.description} price={meal.price} />
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
   ));
   return (
     <section className={classess.meals}>
